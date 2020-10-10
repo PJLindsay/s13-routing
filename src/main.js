bc4abcaf1e3d1982dd2a4c6a85bfbbe614fa7700
PJ Lindsay <pjlindsay@gmail.com>
2020-10-09 18:30:01 -0600

    Redirect and catch-all route

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
 
 const router = new createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList }, // domain-name.com/teams ==> ...
+    { path: '/', redirect: '/teams' }, // redirect from root
+    { path: '/teams', component: TeamsList }, // domain-name.com/teams ==> ... we could use alias: '/' here but alias won't change content/url?
     { path: '/users', component: UsersList }, // domain-name.com/users ==> ...
-    { path: '/teams/:teamId', component: TeamMembers, props: true } // dynamic route with parameter
+    { path: '/teams/:teamId', component: TeamMembers, props: true }, // dynamic route with parameter
+    // { path: '/:notFound(.*)', redirect: '/teams' } // dynamic segment catch any route not handled - must be LAST in routes list
+    { path: '/:notFound(.*)', component: NotFound } // dynamic segment catch any route not handled - must be LAST in routes list
   ],
   linkActiveClass: 'active'
 });
